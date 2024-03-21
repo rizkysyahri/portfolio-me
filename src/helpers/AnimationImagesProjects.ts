9dec90e68094155878b7894bd254b481e6170eee
Rizky Syahri Ramadhan <114801375+rizkysyahri@users.noreply.github.com>
2024-03-22 02:53:53 +0700

    Fix: animate remove (#20)
    * fix(scroll): fix bug doesn't scroll in mobile
    
    * fix(scroll): add data-scroll-container for animate
    
    * Update about page
    
    * fix(scroll): trying bug scroll
    
    * fix(scroll): trying bug scroll on about and project page
    
    * fix(scroll): trying css locomotive scroll
    
    * fix(scroll): scroll doesn't work in mobile
    
    * fix(scroll): trying smoothTouch feature
    
    * fix(scroll): remove overflow-hidden
    
    * fix(scroll): responsive animate
    
    * fix(scroll): scollbar custom
    
    * fix(scroll): remove overflow
    
    * fix(scroll): resolve scroll
    
    * fix(scroll): resolved scroll in mobile
    
    * new amimation
    
    * fix: animate remove
    
    * fix: animate

--- src/helpers/AnimationImagesProjects.ts
+++ src/helpers/AnimationImagesProjects.ts
@@ -52,7 +52,6 @@ export const scaleCenterImage = (
   return tl;
 };
 
-
 export const setBackgroundState = (centerSectionRef: any) => {
   gsap.set(centerSectionRef, { scale: 1.5 });
 };
@@ -61,7 +60,6 @@ export const moveBackgroundProject = (
   centerSectionWrapperRef: any,
   centerSectionRef: any,
   rightImageRef: any
-
 ) => {
   const tl = gsap.timeline();
   const portraitQuery = window.matchMedia("(orientation: portrait)");
@@ -69,7 +67,6 @@ export const moveBackgroundProject = (
   tl.to(centerSectionWrapperRef, {
     width: "100%",
     height: "100vh",
-
   })
     .to(
       centerSectionRef,
@@ -87,15 +84,6 @@ export const moveBackgroundProject = (
       },
       0
     );
-
-  }).to(
-    centerSectionRef,
-    {
-      scale: 1,
-    },
-    0
-  );
-
 
   const handleOrientationChange = () => {
     if (portraitQuery.matches) {
