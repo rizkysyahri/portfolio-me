74de802db8f2afa591b9c12bf6bf4aa0f3aaaadc
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2024-07-23 15:48:58 +0700

    first commit

---
+++ src/providers/data/projectData.ts
@@ -0,0 +1,149 @@
+export const project = [
+  {
+    id: 1,
+    slug: "circle-app",
+    sort: "01",
+    imageProject1: [
+      {
+        id: 10,
+        image: "/circle-app.jpeg",
+      },
+      {
+        id: 11,
+        image: "/circle-app.jpeg",
+      },
+    ],
+
+    imageProject2: "/circle-app.jpeg",
+    imageProjectMock: "/images/mock-circle.png",
+    imageTitle: "/images/SWALLET APP.png",
+    imageAboutTitle: "/images/modernweb.png",
+    imageSection: [
+      {
+        id: 10,
+        image: "/images/circle1.jpeg",
+      },
+      {
+        id: 11,
+        image: "/images/circle2.jpeg",
+      },
+      {
+        id: 12,
+        image: "/images/circle3.jpeg",
+      },
+      {
+        id: 13,
+        image: "/images/circle4.jpeg",
+      },
+    ],
+    imageColumn: [
+      {
+        image: "/images/circle1.jpeg",
+      },
+      {
+        image: "/images/circle2.jpeg",
+      },
+      {
+        image: "/images/circle3.jpeg",
+      },
+      {
+        image: "/images/circle4.jpeg",
+      },
+      {
+        image: "/images/circle2.jpeg",
+      },
+      {
+        image: "/images/circle1.jpeg",
+      },
+      {
+        image: "/images/circle3.jpeg",
+      },
+      {
+        image: "/images/circle1.jpeg",
+      },
+      {
+        image: "/images/circle2.jpeg",
+      },
+    ],
+    title: "CIRCLE APP",
+    url: "",
+    urlGithub: "https://github.com/rizkysyahri/ui-circle-app",
+    description:
+      "Circle App is a social networking platform designed to foster connections and conversations, much like X (formerly known as Twitter). It allows users to share updates, follow topics of interest, and engage with a community of like-minded individuals.  ",
+  },
+  {
+    id: 2,
+    slug: "swallet-app",
+    sort: "02",
+    imageProject1: [
+      {
+        id: 12,
+        image: "/swallet.jpeg",
+      },
+      {
+        id: 13,
+        image: "/swallet.jpeg",
+      },
+    ],
+    imageProject2: "/swallet.jpeg",
+    imageProjectMock: "/images/ilustration1.png",
+    imageTitle: "/images/SWALLET APP.png",
+    imageAboutTitle: "/images/modernweb.png",
+    imageSection: [
+      {
+        id: 5,
+        image: "/images/swallet1.jpeg",
+      },
+      {
+        id: 6,
+        image: "/images/swallet2.jpeg",
+      },
+      {
+        id: 7,
+        image: "/images/swallet3.jpeg",
+      },
+      {
+        id: 8,
+        image: "/images/swallet4.jpeg",
+      },
+      {
+        id: 9,
+        image: "/images/swallet5.jpeg",
+      },
+    ],
+    imageColumn: [
+      {
+        image: "/images/swallet1.jpeg",
+      },
+      {
+        image: "/images/swallet2.jpeg",
+      },
+      {
+        image: "/images/swallet3.jpeg",
+      },
+      {
+        image: "/images/swallet4.jpeg",
+      },
+      {
+        image: "/images/mock-swallet.png",
+      },
+      {
+        image: "/images/swallet2.jpeg",
+      },
+      {
+        image: "/images/swallet3.jpeg",
+      },
+      {
+        image: "/images/swallet4.jpeg",
+      },
+      {
+        image: "/images/swallet1.jpeg",
+      },
+    ],
+    title: "SWALLET APP",
+    url: "https://swallet-web.vercel.app/",
+    urlGithub: "https://github.com/rizkysyahri/swallet-web",
+    description:
+      "Swallet App is your ultimate solution for managing personal finances effortlessly. With Swallet, you can track expenses, monitor budgets, and gain insights into your spending habits, all in a user-friendly interface. Stay in control of your finances with Swallet App.",
+  },
+];
